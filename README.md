3104b85067a06e43a665bd7f92898cef2ca9867f
Corey Alix <coreyalix@hotmail.com>
2017-02-20 13:53:16 -0500

    V4.0.1 (#1)
    * Introduces ol3-draw
    
    * built files

---
+++ README.md
@@ -0,0 +1,18 @@
+# OpenLayers Feature Editing
+* [ol3-draw](https://github.com/ca0v/ol3-draw).
+
+## Examples
+
+* [v4.0.1](https://rawgit.com/ca0v/ol3-draw/v4.0.1/rawgit.html)
+* [master](https://rawgit.com/ca0v/ol3-draw/master/rawgit.html)
+
+## Build
+
+* typings install
+* bower install
+* tsc -w
+
+## Consume
+
+* `typings install ol3-draw=github:ca0v/ol3-draw/built/index.d.ts#v4.0.1 --global --save`
+* `bower install ol3-draw=git://github.com/ca0v/ol3-draw.git#v4.0.1 --save`
---
+++ ol3-draw/examples/index.ts
@@ -0,0 +1,33 @@
+export function run() {
+    let l = window.location;
+    let path = `${l.origin}${l.pathname}?run=ol3-draw/examples/`;
+    let labs = `
+    index
+    ol3-draw
+    `;
+
+    let styles = document.createElement("style");
+    document.head.appendChild(styles);
+
+    styles.innerText += `
+    #map {
+        display: none;
+    }
+    .test {
+        margin: 20px;
+    }
+    `;
+
+    let html = labs
+        .split(/ /)
+        .map(v => v.trim())
+        .filter(v => !!v)
+        //.sort()
+        .map(lab => `<div class='test'><a href='${path}${lab}&debug=0'>${lab}</a></div>`)
+        .join("\n");
+
+
+    html += `<a href='${l.origin}${l.pathname}?run=ol3-draw/tests/index'>tests</a>`;
+
+    document.write(html);
+};
