15ca2a3edcd9262e17e1908af8176b3e194bb6c7
coreyalix <coreyalix@hotmail.com>
2017-03-04 15:56:53 -0500

    adds selection box to delete

--- ol3-draw/ol3-delete.ts
+++ ol3-draw/ol3-delete.ts
@@ -1,8 +1,12 @@
 import ol = require("openlayers");
 import { Button, ButtonOptions as IButtonOptions } from "./ol3-button";
-import { mixin } from "ol3-fun/ol3-fun/common";
+import { defaults } from "ol3-fun/ol3-fun/common";
+import { Format } from "ol3-symbolizer";
 
 export interface DeleteControlOptions extends IButtonOptions {
+    multi?: boolean;
+    style?: { [name: string]: Format.Style[] };
+    boxSelectCondition?: (mapBrowserEvent: ol.MapBrowserEvent) => boolean;
 }
 
 export class Delete extends Button {
@@ -12,26 +16,85 @@ export class Delete extends Button {
         label: "␡",
         title: "Delete",
         buttonType: Delete,
-        eventName: "delete-feature"
+        eventName: "delete-feature",
+        boxSelectCondition: ol.events.condition.shiftKeyOnly,
+        style: {
+            "Point": [{
+                circle: {
+                    radius: 20,
+                    fill: {
+                        color: "blue"
+                    },
+                    stroke: {
+                        color: "red",
+                        width: 2
+                    },
+                    opacity: 1
+                }
+            }],
+            "MultiLineString": [{
+                stroke: {
+                    color: "red",
+                    width: 2
+                }
+            }],
+            "Circle": [{
+                fill: {
+                    color: "blue"
+                },
+                stroke: {
+                    color: "red",
+                    width: 2
+                }
+            }],
+            "Polygon": [{
+                fill: {
+                    color: "blue"
+                },
+                stroke: {
+                    color: "red",
+                    width: 2
+                }
+            }],
+            "MultiPolygon": [{
+                fill: {
+                    color: "blue"
+                },
+                stroke: {
+                    color: "red",
+                    width: 2
+                }
+            }]
+        }
     }
 
     public options: DeleteControlOptions;
 
     static create(options?: DeleteControlOptions) {
-        options = mixin(mixin({}, Delete.DEFAULT_OPTIONS), options);
+        options = defaults({}, options, Delete.DEFAULT_OPTIONS);
         return Button.create(options);
     }
 
+    private featureLayerAssociation_: any;
+
+    private addFeatureLayerAssociation(feature: ol.Feature, layer: ol.layer.Vector) {
+        if (!this.featureLayerAssociation_) this.featureLayerAssociation_ = [];
+        var key = feature.getId();
+        this.featureLayerAssociation_[key] = layer;
+    }
 
     constructor(options: DeleteControlOptions) {
         super(options);
 
         let map = options.map;
-        let select = new ol.interaction.Select({
-            wrapX: false,
+        let featureLayers = <Array<{ feature: ol.Feature; source: ol.source.Vector }>>[];
+
+        let selection = new ol.interaction.Select({
+            condition: ol.events.condition.click,
+            multi: false,
             style: (feature: ol.Feature, res: number) => {
 
-                let index = select.getFeatures().getArray().indexOf(feature);
+                let index = selection.getFeatures().getArray().indexOf(feature);
 
                 let fillColor = "rgba(0,0,0,0.2)";
                 let strokeColor = "red";
@@ -48,65 +111,55 @@ export class Delete extends Button {
                     scale: 3
                 };
 
-                switch (feature.getGeometry().getType()) {
-                    case "Point":
-                        return this.symbolizer.fromJson({
-                            circle: {
-                                radius: 20,
-                                fill: {
-                                    color: fillColor
-                                },
-                                stroke: {
-                                    color: strokeColor,
-                                    width: 2
-                                },
-                                opacity: 1
-                            },
-                            text: textTemplate
-                        });
-                    case "MultiLineString":
-                        return this.symbolizer.fromJson({
-                            stroke: {
-                                color: strokeColor,
-                                width: 2
-                            },
-                            text: textTemplate
+                let style = options.style[feature.getGeometry().getType()]
+                    .map(s => this.symbolizer.fromJson(defaults({ text: textTemplate }, s)));
 
-                        });
-                    case "Circle":
-                    case "Polygon":
-                    case "MultiPolygon":
-                        return this.symbolizer.fromJson({
-                            fill: {
-                                color: fillColor
-                            },
-                            stroke: {
-                                color: strokeColor,
-                                width: 2
-                            },
-                            text: textTemplate
-                        });
-                    default:
-                        debugger;
-                }
+                return style;
             }
         });
 
+        let boxSelect = new ol.interaction.DragBox({
+            condition: options.boxSelectCondition
+        });
+
+        boxSelect.on("boxend", args => {
+            let extent = boxSelect.getGeometry().getExtent();
+            let features = selection.getFeatures().getArray();
+            options.map.getLayers()
+                .getArray()
+                .filter(l => l instanceof ol.layer.Vector)
+                .map(l => <ol.layer.Vector>l)
+                .forEach(l => l.getSource().forEachFeatureIntersectingExtent(extent, feature => {
+                    if (-1 === features.indexOf(feature)) {
+                        selection.getFeatures().push(feature);
+                        this.addFeatureLayerAssociation(feature, l);
+                    } else {
+                        selection.getFeatures().remove(feature);
+                        this.addFeatureLayerAssociation(feature, null);
+                    }
+                }));
+        });
+
         let doit = () => {
-            select.getFeatures().forEach(f => {
-                let l = select.getLayer(f);
-                l.getSource().removeFeature(f);
+            selection.getFeatures().forEach(f => {
+                let l = selection.getLayer(f) || this.featureLayerAssociation_[f.getId()];
+                l && l.getSource().removeFeature(f);
             });
-            select.getFeatures().clear();
+            selection.getFeatures().clear();
+            this.featureLayerAssociation_ = [];
         }
 
         this.once("change:active", () => {
-            select.setActive(false);
-            map.addInteraction(select);
+            [selection, boxSelect].forEach(i => {
+                i.setActive(false);
+                map.addInteraction(i);
+            });
 
             this.handlers.push(() => {
-                select.setActive(false);
-                map.removeInteraction(select);
+                [selection, boxSelect].forEach(i => {
+                    i.setActive(false);
+                    map.removeInteraction(i);
+                });
             });
         });
 
@@ -114,9 +167,9 @@ export class Delete extends Button {
             let active = this.get("active");
             if (!active) {
                 doit();
-                select.getFeatures().clear();
+                selection.getFeatures().clear();
             }
-            select.setActive(active);
+            [boxSelect, selection].forEach(i => i.setActive(active));
         });
 
     }
